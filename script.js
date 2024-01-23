cdd86ab0d656d4614b3d3c784c990c65d3ae1229
Chris <cwmichea@uc.cl>
2024-01-22 23:11:36 -0500

    1 commit

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ class Calculator{
         this.currOp = this.currOp.toString().slice(0, -1)
     }
     AppendNumber(number){
-        if(number === '.' && this.currOp.includes('.') ) return
+        if(number === '.' && this.currOp.includes('.') ) return //ignore if there is already a point
         this.currOp = this.currOp.toString() + number.toString()
     }
     ChooseOp(operation){
-        if(this.currOp === '') return
+        if(this.currOp === '') return //ignore if there is no first number
         // Check if the currentOp, meaning current number, is nothing, do nothing
-        if(this.prevOp !== '') {this.Compute() } //3 numbers situation
+        if(this.prevOp !== '') {this.Compute() } // calcule when there already 2 number and we add a 3rd numbers, then calcule what's already there (3 num situation)
         // Once you have alerady 2 number with an operation
         // well compute and now you keep adding an operation, we proceed
         this.operation = operation
@@ -58,35 +58,28 @@ class Calculator{
         const floatNum = parseFloat(number)
         if (isNaN(floatNum)) return ''
         return floatNum.toLocaleString('es')
-
     }
  
     UpdateDisplay(){
         this.currOpTextElement.innerText = this.getDisplay(this.currOp)
         if (this.operation != null) { 
             this.prevOpTextElement.innerText = 
-            `${this.getDisplay(this.prevOp)} ${this.operation}`
+            `${this.getDisplay(this.prevOp)} ${this.operation}`//add a point each 3 digits
         } else {
-            this.prevOpTextElement.innerText = ''
+            this.prevOpTextElement.innerText = ''// once a operator has been picked, free this space to write a new number to add to the calculation 
         }//this is to clean the sceen once you realize the operation
         // this.prevOpTextElement.innerText = this.prevOp
     }
-
-    // UpdateDisplay(){
-    //     this.currOpTextElement.innerText = this.currOp
-    //     if (this.operation != null) {
-    //         this.prevOpTextElement.innerText = `${this.prevOp} ${this.operation}`
-    //     }
-    //     // this.prevOpTextElement.innerText = this.prevOp
-    // }
-
 }
 // properties
+//number and operation buttons (adding string)
 const numberButtons    = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
+//calculate or so sth (editting data)
 const equalButton = document.querySelector('[data-equal]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-allclear]')
+//string to see what is going on
 const prevOpTextElement = document.querySelector('[data-prevop]')
 const currOpTextElement = document.querySelector('[data-currop]')
 // code
